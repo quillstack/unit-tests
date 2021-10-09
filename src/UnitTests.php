b692d28d744dda443bca193f305dd61b04db7368
Radoslaw Ziemniewicz <admin@verbanent.com>
2021-10-09 14:40:37 +0100

    Added test coverage and updated Sonar config

--- src/UnitTests.php
+++ src/UnitTests.php
@@ -4,6 +4,8 @@ namespace Quillstack\UnitTests;
 
 use Exception;
 use Quillstack\DI\Container;
+use Quillstack\LocalStorage\LocalStorage;
+use Quillstack\StorageInterface\StorageInterface;
 
 class UnitTests
 {
@@ -11,11 +13,15 @@ class UnitTests
 
     public function __construct(private array $tests = [])
     {
-        $this->di = new Container();
+        $this->di = new Container([
+            StorageInterface::class => LocalStorage::class,
+        ]);
     }
 
     public function run(): void
     {
+        \phpdbg_start_oplog();
+
         foreach ($this->tests as $test) {
             echo $test, ':', PHP_EOL;
             $testObject = $this->di->get($test);
@@ -43,5 +49,51 @@ class UnitTests
 
             echo PHP_EOL;
         }
+
+        $data = \phpdbg_end_oplog();
+        $result = [];
+        $output = [];
+
+        foreach ($data as $file => $coverage) {
+            if (!str_starts_with($file, __DIR__)) {
+                continue;
+            }
+
+            foreach ($coverage as $line => $value) {
+                $result[$file][$line] = $value <= 0 ? 0 : 1;
+            }
+        }
+
+        $lines = phpdbg_get_executable();
+
+        foreach ($lines as $file => $coverage) {
+            if (!str_starts_with($file, __DIR__)) {
+                continue;
+            }
+
+            foreach ($coverage as $line => $value) {
+                if (isset($result[$file][$line])) {
+                    $output[$file][$line] = $result[$file][$line];
+                } else {
+                    $output[$file][$line] = $value;
+                }
+            }
+        }
+
+        $xml = new \SimpleXMLElement('<xml/>');
+        $coverage = $xml->addChild('coverage');
+        $project = $coverage->addChild('project');
+        foreach ($output as $file => $lines) {
+            $class = $project->addChild('file');
+            $class->addAttribute('name', $file);
+            foreach ($lines as $line => $value) {
+                $row = $class->addChild('line');
+                $row->addAttribute('num', $line);
+                $row->addAttribute('count', $value);
+            }
+        }
+
+        $storage = $this->di->get(StorageInterface::class);
+        $storage->save(__DIR__ . '/../unit-tests.coverage.xml', $xml->asXML());
     }
 }
