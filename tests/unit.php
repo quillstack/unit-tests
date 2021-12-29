4319a438ea1f451c3e9484cffbc8ab2d9c2ccf85
Radek Ziemniewicz <radek@quillstack.org>
2021-12-29 22:30:28 +0000

    Enabled all tests

--- tests/unit.php
+++ tests/unit.php
@@ -3,5 +3,10 @@
 declare(strict_types=1);
 
 return [
+    \Quillstack\UnitTests\Tests\Unit\TestAssertEqual::class,
+    \Quillstack\UnitTests\Tests\Unit\TestExceptionExpectation::class,
+
+    \Quillstack\UnitTests\Tests\Unit\Types\TestAssertArrays::class,
+    \Quillstack\UnitTests\Tests\Unit\Types\TestAssertBooleans::class,
     \Quillstack\UnitTests\Tests\Unit\Types\TestAssertObjects::class,
 ];
