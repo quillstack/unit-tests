76f2282074a1fed28af9bb70b9ad61b614609c29
Radek Ziemniewicz <radek@quillstack.org>
2021-12-31 00:38:55 +0000

    Added checking if object is not null

--- src/Types/AssertObject.php
+++ src/Types/AssertObject.php
@@ -1,17 +1,29 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Quillstack\UnitTests\Types;
 
 use Quillstack\UnitTests\Exceptions\Types\Objects\ObjectIsNotInstanceOfClassException;
+use Quillstack\UnitTests\Exceptions\Types\Objects\ObjectIsNullException;
 
 class AssertObject
 {
-    public function instanceOf(string $instance, object $object)
+    public function instanceOf(string $instance, object $object): void
     {
         if ($object instanceof $instance) {
             return;
         }
 
         throw new ObjectIsNotInstanceOfClassException("Expected object of {$instance}");
+    }
+
+    public function notNull(?object $object): void
+    {
+        if ($object !== null) {
+            return;
+        }
+
+        throw new ObjectIsNullException();
     }
 }
