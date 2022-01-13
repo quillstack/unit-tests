b2b42b7300dafa795ada0fa23619c3a1a00ad726
Radek Ziemniewicz <radek@quillstack.org>
2022-01-13 19:39:47 +0000

    Fixed assert equal objects

---
+++ src/Exceptions/Types/Objects/ObjectValuesNotEqualException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Quillstack\UnitTests\Exceptions\Types\Objects;
+
+use Quillstack\UnitTests\Exceptions\UnitTestsException;
+
+class ObjectValuesNotEqualException extends UnitTestsException
+{
+    //
+}
